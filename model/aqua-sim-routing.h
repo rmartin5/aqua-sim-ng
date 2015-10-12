1fb3e7ee2cd60fbd16e0db9c49a4f7d5609d20f5
rmartin5 <martinr521@gmail.com>
2015-10-12 13:34:07 -0400

    TODO

--- model/aqua-sim-routing.h
+++ model/aqua-sim-routing.h
@@ -30,36 +30,36 @@ class AquaSimRouting : public Object
 {	
 public:	
 /*define common commands for underwater routing protocols*/
-	static TypeId GetTypeId(void);
-	AquaSimRouting(void);
-	virtual ~AquaSimRouting(void);
+  static TypeId GetTypeId(void);
+  AquaSimRouting(void);
+  virtual ~AquaSimRouting(void);
 
-	/*avoid instantiation since UnderwaterRouting's behavior is not defined*/
-	virtual void Recv(Ptr<Packet>) = 0;	//handler not implemented
+  /*avoid instantiation since UnderwaterRouting's behavior is not defined*/
+  virtual void Recv(Ptr<Packet>) = 0;	//handler not implemented
 protected:
-	/*send packet up to port-demux*/
-	virtual void SendUp(Ptr<Packet> p);			//demux not implemented yet.
-	/*send packet p to next_hop after delay*/
-	virtual void SendDown(Ptr<Packet> p, Address &nextHop, Time delay);
-	/*check if if a dead loop results in the incoming packet*/
-	virtual bool IsDeadLoop(Ptr<Packet> p);
-	/*check if this node is the next hop*/
-	virtual bool AmINextHop(const Ptr<Packet> p);
-	/*check if this node is the destination.*/
-	virtual bool AmIDst(const Ptr<Packet> p);
-	/*check if this node is the source node,
-		* i.e., whose app layer generates this packet.*/
-	virtual bool AmISrc(const Ptr<Packet> p);
+  /*send packet up to port-demux*/
+  virtual void SendUp(Ptr<Packet> p);			//demux not implemented yet.
+  /*send packet p to next_hop after delay*/
+  virtual void SendDown(Ptr<Packet> p, Address &nextHop, Time delay);
+  /*check if if a dead loop results in the incoming packet*/
+  virtual bool IsDeadLoop(Ptr<Packet> p);
+  /*check if this node is the next hop*/
+  virtual bool AmINextHop(const Ptr<Packet> p);
+  /*check if this node is the destination.*/
+  virtual bool AmIDst(const Ptr<Packet> p);
+  /*check if this node is the source node,
+	  * i.e., whose app layer generates this packet.*/
+  virtual bool AmISrc(const Ptr<Packet> p);
 protected:
-	Address m_myAddr;  //the ip address of this node
-	Ptr<AquaSimNode> m_node; //the host node
-	//Ptr<Trace> m_traceTarget;       // Trace Target	TODO need to initiate tracing
-	//NsObject *ll;			//pointer to link layer object
-	//NsObject *port_dmux;
+  Address m_myAddr;  //the ip address of this node
+  Ptr<AquaSimNode> m_node; //the host node
+  //Ptr<Trace> m_traceTarget;       // Trace Target	TODO need to initiate tracing
+  //NsObject *ll;			//pointer to link layer object
+  //NsObject *port_dmux;
 
 private:
-	AquaSimPacketStamp* m_pStamp;
-	Ptr<AquaSimMac> m_mac;
+  AquaSimPacketStamp* m_pStamp;
+  Ptr<AquaSimMac> m_mac;
 
 };  //AquaSimRouting class
 
