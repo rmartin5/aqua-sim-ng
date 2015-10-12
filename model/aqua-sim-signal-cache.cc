33cac5e0eb1dc7d3559631d1dd0e8673b89a9898
rmartin5 <martinr521@gmail.com>
2015-10-12 13:42:12 -0400

    bug fixes

--- model/aqua-sim-signal-cache.cc
+++ model/aqua-sim-signal-cache.cc
@@ -20,43 +20,43 @@ NS_OBJECT_ENSURE_REGISTERED(PktSubmissionTimer);
 
 PktSubmissionTimer::PktSubmissionTimer(AquaSimSignalCache* sC)
 {
-	m_sC = sC;
+  m_sC = sC;
 }
 
 PktSubmissionTimer::~PktSubmissionTimer()
 {
-	NS_LOG_FUNCTION(this);
+  NS_LOG_FUNCTION(this);
 }
 
 TypeId
 PktSubmissionTimer::GetTypeId (void)
 {
-	static TypeId tid = TypeId ("ns3::PktSubmissionTimer")
-	;
-	return tid;
+  static TypeId tid = TypeId ("ns3::PktSubmissionTimer")
+  ;
+  return tid;
 }
 
 void
 PktSubmissionTimer::Expire(void) {
-	IncomingPacket* inPkt = m_waitingList.top().inPkt;
-	m_waitingList.pop();
+  IncomingPacket* inPkt = m_waitingList.top().inPkt;
+  m_waitingList.pop();
 
-	if (!m_waitingList.empty()) {
-		Simulator::Schedule( m_waitingList.top().endT - Simulator::Now(), &PktSubmissionTimer::Expire, this);
-	}
-	m_sC->SubmitPkt(inPkt);
+  if (!m_waitingList.empty()) {
+    Simulator::Schedule( m_waitingList.top().endT - Simulator::Now(), &PktSubmissionTimer::Expire, this);
+  }
+  m_sC->SubmitPkt(inPkt);
 }
 
 void
 PktSubmissionTimer::AddNewSubmission(IncomingPacket* inPkt) {
-	AquaSimHeader asHeader;	
-	(inPkt->packet)->PeekHeader(asHeader);
-	NS_LOG_DEBUG ("txtime=" << asHeader.GetTxTime());
-	Time endT_ = Time(Simulator::Now().GetSeconds() + asHeader.GetTxTime());
-	if (m_waitingList.empty() || m_waitingList.top().endT > endT_) {
-		Simulator::Schedule( endT_ - Simulator::Now(), &PktSubmissionTimer::AddNewSubmission, this, inPkt );
-	}
-	m_waitingList.push(PktSubmissionUnit(inPkt, endT_));
+  AquaSimHeader asHeader;
+  (inPkt->packet)->PeekHeader(asHeader);
+  NS_LOG_DEBUG ("txtime=" << asHeader.GetTxTime());
+  Time endT_ = Time(Simulator::Now().GetSeconds() + asHeader.GetTxTime());
+  if (m_waitingList.empty() || m_waitingList.top().endT > endT_) {
+    Simulator::Schedule( endT_ - Simulator::Now(), &PktSubmissionTimer::AddNewSubmission, this, inPkt );
+  }
+  m_waitingList.push(PktSubmissionUnit(inPkt, endT_));
 }
 
 
@@ -70,139 +70,140 @@ NS_OBJECT_ENSURE_REGISTERED(AquaSimSignalCache);
 AquaSimSignalCache::AquaSimSignalCache() :
 m_pktNum(0), m_totalPS(0.0), m_phy(NULL), m_pktSubTimer(NULL)
 {
-	m_head = new IncomingPacket;
-	m_pktSubTimer = new PktSubmissionTimer(this);
+  m_head = new IncomingPacket;
+  m_pktSubTimer = new PktSubmissionTimer(this);
+  status = INVALID;
 }
 
 AquaSimSignalCache::~AquaSimSignalCache() {
-	IncomingPacket* pos = m_head;
-	while (m_head != NULL) {
-		m_head = m_head->next;
-		pos->packet = 0;
-		pos = 0;		//this is probably memory leakage.
-		pos = m_head;
-	}
+  IncomingPacket* pos = m_head;
+  while (m_head != NULL) {
+    m_head = m_head->next;
+    pos->packet = 0;
+    pos = 0;		//this is probably memory leakage.
+    pos = m_head;
+  }
 
-	m_pktSubTimer = 0;	//Should probably use unref instead...
+  m_pktSubTimer = 0;	//Should probably use unref instead...
 }
 
 TypeId
 AquaSimSignalCache::GetTypeId (void)
 {
-	static TypeId tid = TypeId ("ns3::AquaSimSignalCache")
-		//.SetParent<Object> ();
-	;
-	return tid;
+  static TypeId tid = TypeId ("ns3::AquaSimSignalCache")
+	  //.SetParent<Object> ();
+  ;
+  return tid;
 }
 
 void 
 AquaSimSignalCache::AddNewPacket(Ptr<Packet> p){
-	/**
-	* any packet error marked before this step means
-	* this packet is invalid and will be considered
-	* as noise to other packets only.
-	*/
+  /**
+  * any packet error marked before this step means
+  * this packet is invalid and will be considered
+  * as noise to other packets only.
+  */
 
-	AquaSimHeader asHeader;	
-	p->PeekHeader(asHeader);
-	NS_LOG_DEBUG ("errorFlag=" << asHeader.GetErrorFlag());
+  AquaSimHeader asHeader;
+  p->PeekHeader(asHeader);
+  NS_LOG_DEBUG ("errorFlag=" << asHeader.GetErrorFlag());
 
-	IncomingPacket* inPkt = new IncomingPacket(PeekPointer(p),
-			asHeader.GetErrorFlag() ? INVALID : RECEPTION);
+  IncomingPacket* inPkt = new IncomingPacket(PeekPointer(p),
+		  asHeader.GetErrorFlag() ? INVALID : RECEPTION);
 
-	m_pktSubTimer->AddNewSubmission(inPkt);
+  m_pktSubTimer->AddNewSubmission(inPkt);
 
-	inPkt->next = m_head->next;
-	m_head->next = inPkt;		
+  inPkt->next = m_head->next;
+  m_head->next = inPkt;
 
-	m_pktNum++;
-	m_totalPS += m_em->GetRxPower();
-	UpdatePacketStatus();
+  m_pktNum++;
+  m_totalPS += m_em->GetRxPower();
+  UpdatePacketStatus();
 }
 
 
 bool
 AquaSimSignalCache::DeleteIncomingPacket(Ptr<Packet> p){
-	IncomingPacket* pre = m_head;
-	IncomingPacket* ptr = m_head->next;
+  IncomingPacket* pre = m_head;
+  IncomingPacket* ptr = m_head->next;
 
-	while (ptr != NULL && ptr->packet != p) {
-		ptr = ptr->next;
-		pre = pre->next;
-	}
+  while (ptr != NULL && ptr->packet != p) {
+    ptr = ptr->next;
+    pre = pre->next;
+  }
 
-	if (ptr != NULL && ptr->packet == p) {
-		m_pktNum--;
-		m_totalPS -= m_em->GetRxPower();
-		pre->next = ptr->next;
-		ptr = 0;
-		return true;
-	}
-	return false;
+  if (ptr != NULL && ptr->packet == p) {
+    m_pktNum--;
+    m_totalPS -= m_em->GetRxPower();
+    pre->next = ptr->next;
+    ptr = 0;
+    return true;
+  }
+  return false;
 }
 
 
 void
 AquaSimSignalCache::SubmitPkt(IncomingPacket* inPkt) {
-	status = inPkt->status;
-	Ptr<Packet> p = inPkt->packet;
-	DeleteIncomingPacket(p); //object pointed by inPkt is deleted here
-	/**
-	* modem has no idea about invalid packets, so release
-	* them here
-	*/
-	if (status == INVALID)
-		p = 0;
-	else
-		m_phy->SignalCacheCallback(p);
+  status = inPkt->status;
+  Ptr<Packet> p = inPkt->packet;
+  DeleteIncomingPacket(p); //object pointed by inPkt is deleted here
+  /**
+  * modem has no idea about invalid packets, so release
+  * them here
+  */
+  if (status == INVALID)
+    p = 0;
+  else
+    m_phy->SignalCacheCallback(p);
 }
 
 
 IncomingPacket*
 AquaSimSignalCache::Lookup(Ptr<Packet> p){
-	IncomingPacket* ptr = m_head->next;
+  IncomingPacket* ptr = m_head->next;
 
-	while (ptr != NULL && ptr->packet != PeekPointer(p))
-		ptr = ptr->next;
-	return ptr;
+  while (ptr != NULL && ptr->packet != PeekPointer(p)) {
+    ptr = ptr->next;
+  }
+  return ptr;
 }
 
 
 void
 AquaSimSignalCache::InvalidateIncomingPacket(){
-	IncomingPacket* ptr = m_head->next;
+  IncomingPacket* ptr = m_head->next;
 
-	while (ptr != NULL) {
-		ptr->status = INVALID;
-		ptr = ptr->next;
-	}
+  while (ptr != NULL) {
+    ptr->status = INVALID;
+    ptr = ptr->next;
+  }
 }
 
 
 PacketStatus 
 AquaSimSignalCache::Status(Ptr<Packet> p){
-	IncomingPacket* ptr = Lookup(p);
+  IncomingPacket* ptr = Lookup(p);
 
-	return ptr == NULL ? INVALID : ptr->status;
+  return ptr == NULL ? INVALID : ptr->status;
 }
 
 
 void
 AquaSimSignalCache::UpdatePacketStatus(){
+  double noise = 0,		//total noise
+	 ps = 0;		//power strength
+	//,SINR = 0; 		//currently not used
 
-	double	noise = 0,		//total noise
-		ps = 0;			//power strength
-		//,SINR = 0; 		//currently not used
+  for (IncomingPacket* ptr = m_head->next; ptr != NULL; ptr = ptr->next) {
+    ps = m_em->GetRxPower();
+    noise = m_totalPS - ps;
 
-	for (IncomingPacket* ptr = m_head->next; ptr != NULL; ptr = ptr->next) {
-		ps = m_em->GetRxPower();
-		noise = m_totalPS - ps;
+    if (ptr->status != RECEPTION)
+      continue;
 
-		if (ptr->status != RECEPTION)
-			continue;
-
-		ptr->status = m_phy->Decodable(noise + m_noise->Noise(), ps) ? RECEPTION : INVALID;
-	}
+    ptr->status = m_phy->Decodable(noise + m_noise->Noise(), ps) ? RECEPTION : INVALID;
+  }
 }
 
 
--- model/aqua-sim-signal-cache.h
+++ model/aqua-sim-signal-cache.h
@@ -24,25 +24,25 @@ class Packet;
 enum PacketStatus{ RECEPTION, COLLISION, INVALID };
 
 struct IncomingPacket{
-	Packet* packet;
-	enum PacketStatus status;
-	IncomingPacket* next;
-	IncomingPacket(Packet* p = NULL, PacketStatus s = INVALID) :
-		packet(p), status(s), next(NULL) {}
+  Packet* packet;
+  enum PacketStatus status;
+  IncomingPacket* next;
+  IncomingPacket(Packet* p = NULL, PacketStatus s = INVALID) :
+	  packet(p), status(s), next(NULL) {}
 };
 
 class AquaSimPhy;
 class AquaSimSignalCache;
 
 struct PktSubmissionUnit{
-	IncomingPacket* inPkt;
-	Time endT;	//ending time of reception
-	friend bool operator < (const PktSubmissionUnit &l, const PktSubmissionUnit &r) {
-		//pkt with earlier ending time appears at the front
-		return l.endT >= r.endT;
-	}
+  IncomingPacket* inPkt;
+  Time endT;	//ending time of reception
+  friend bool operator < (const PktSubmissionUnit &l, const PktSubmissionUnit &r) {
+    //pkt with earlier ending time appears at the front
+    return l.endT >= r.endT;
+  }
 	
-	PktSubmissionUnit(IncomingPacket* inPkt_, Time endT_);
+  PktSubmissionUnit(IncomingPacket* inPkt_, Time endT_);
 };
 
 /**
@@ -50,15 +50,15 @@ struct PktSubmissionUnit{
 */
 class PktSubmissionTimer : public Timer{	
 private:
-	std::priority_queue<PktSubmissionUnit> m_waitingList;
-	AquaSimSignalCache* m_sC;
+  std::priority_queue<PktSubmissionUnit> m_waitingList;
+  AquaSimSignalCache* m_sC;
 public:
-	PktSubmissionTimer(AquaSimSignalCache* sC);
-	virtual ~PktSubmissionTimer(void);
-	static TypeId GetTypeId(void);
+  PktSubmissionTimer(AquaSimSignalCache* sC);
+  virtual ~PktSubmissionTimer(void);
+  static TypeId GetTypeId(void);
 
-	virtual void Expire(void);
-	void AddNewSubmission(IncomingPacket* inPkt);
+  virtual void Expire(void);
+  void AddNewSubmission(IncomingPacket* inPkt);
 };  // class PktSubmissionTimer
 
 /**
@@ -67,43 +67,43 @@ public:
 */
 class AquaSimSignalCache : public Object { 
 public:
-	AquaSimSignalCache(void);
-	virtual ~AquaSimSignalCache(void);
-	static TypeId GetTypeId(void);
+  AquaSimSignalCache(void);
+  virtual ~AquaSimSignalCache(void);
+  static TypeId GetTypeId(void);
 
-	virtual void AddNewPacket(Ptr<Packet>);
-	virtual bool DeleteIncomingPacket(Ptr<Packet>);
-	void InvalidateIncomingPacket(void);
-	IncomingPacket* Lookup(Ptr<Packet>);
-	PacketStatus status;
-	PacketStatus Status(Ptr<Packet> p);
-	void SubmitPkt(IncomingPacket* inPkt);
+  virtual void AddNewPacket(Ptr<Packet>);
+  virtual bool DeleteIncomingPacket(Ptr<Packet>);
+  void InvalidateIncomingPacket(void);
+  IncomingPacket* Lookup(Ptr<Packet>);
+  PacketStatus status;
+  PacketStatus Status(Ptr<Packet> p);
+  void SubmitPkt(IncomingPacket* inPkt);
 
-	friend class PktSubmissionTimer;
+  friend class PktSubmissionTimer;
 
 protected:
-	virtual void UpdatePacketStatus(void);
+  virtual void UpdatePacketStatus(void);
 
 public:
-	int m_pktNum;	// number of active incoming packets
-	double m_totalPS; // total power strength of active incoming packets
+  int m_pktNum;	// number of active incoming packets
+  double m_totalPS; // total power strength of active incoming packets
 
 protected:
-	IncomingPacket* m_head;
-	Ptr<AquaSimPhy> m_phy;
-	PktSubmissionTimer* m_pktSubTimer;
-	Ptr<AquaSimEnergyModel> m_em;
-	Ptr<AquaSimConstNoiseGen> m_noise;
+  IncomingPacket* m_head;
+  Ptr<AquaSimPhy> m_phy;
+  PktSubmissionTimer* m_pktSubTimer;
+  Ptr<AquaSimEnergyModel> m_em;
+  Ptr<AquaSimConstNoiseGen> m_noise;
 
 private:
-	/**
-	* hide this interface from other classes, UnderwaterPhy
-	* is the only one can use this method
-	*/
-	void AttachPhy(Ptr<AquaSimPhy> phy) {
-		m_phy = phy;
-	}
-	friend class AquaSimPhy;
+  /**
+  * hide this interface from other classes, UnderwaterPhy
+  * is the only one can use this method
+  */
+  void AttachPhy(Ptr<AquaSimPhy> phy) {
+	  m_phy = phy;
+  }
+  friend class AquaSimPhy;
 };  //class AquaSimSignalCache
 
 
@@ -112,7 +112,7 @@ private:
 */
 class AquaSimMultiPathSignalCache : public AquaSimSignalCache {
 	
-	//TODO
+  //TODO - in future work.
 
 };  //class AquaSimMultiPathSignalCache
 
