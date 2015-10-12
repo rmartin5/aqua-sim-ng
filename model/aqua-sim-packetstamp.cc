66789a3e982ba27502a703d918b148e7d28f948f
rmartin5 <martinr521@gmail.com>
2015-10-12 13:10:11 -0400

    indentation adjustment

--- model/aqua-sim-packetstamp.cc
+++ model/aqua-sim-packetstamp.cc
@@ -18,22 +18,22 @@ m_txRange(-1), m_freq(-1), m_noise(0)
 TypeId
 AquaSimPacketStamp::GetTypeId(void)
 {
-	static TypeId tid = TypeId("ns3::AquaSimPacketStamp")
-		;
-	return tid;
+  static TypeId tid = TypeId("ns3::AquaSimPacketStamp")
+    ;
+  return tid;
 }
 
 bool
 AquaSimPacketStamp::CheckConflict(void)
 {
-	return (m_txRange > 0 && m_pt < 0) || (m_txRange < 0 && m_pt > 0);
+  return (m_txRange > 0 && m_pt < 0) || (m_txRange < 0 && m_pt > 0);
 }
 
 void
 AquaSimPacketStamp::Stamp(Packet * p, double pt, double pr)
 {
-	m_pt = pt;
-	m_pr = pr;
+  m_pt = pt;
+  m_pr = pr;
 }
 
 
