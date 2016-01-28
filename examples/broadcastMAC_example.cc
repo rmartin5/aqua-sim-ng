4b09b172b4a41044c9099fb23a9f89b8292cd361
rmartin5 <martinr521@gmail.com>
2016-01-28 14:21:58 -0500

    update

--- examples/broadcastMAC_example.cc
+++ examples/broadcastMAC_example.cc
@@ -38,17 +38,13 @@ main (int argc, char *argv[])
 
   /*
    * **********
-   * Node is the global combination here. May be helpful to review UAN and how it uses Node but my guess is this: Node stats it automatically adds created nodes to NodeList.
-   * So then by going through Nodelist it can have access to all nodes created. This means I need to make sure that Node can access netdevice (again similar to UAN).
-   * ---may not need to create a child class of Node for underwater reasons, instead may just need to ensure connection between node and net device here... may have to scale back net device
-   * and instead add to node (attaching layers to node instead of net device??? not sure standard here)
+   * Node -> NetDevice -> AquaSimNetDeive -> etc.
+   * Note: Nodelist keeps track of all nodes created.
    * ---Also need to look into id of nodes and assignment of this
    * ---need to look at assignment of address and making it unique per node.
    *
    *
    *  Ensure to use NS_LOG when testing in terminal. ie. ./waf --run broadcastMAC_example NS_LOG=Node=level_all or export 'NS_LOG=*=level_all|prefix_func'
-   *
-   *  Note: both index # is 0 while array number differs within m_device, this holds true for both local list and general node.cc list... Good to konw...
    *  *********
    */
 
@@ -172,40 +168,3 @@ main (int argc, char *argv[])
   std::cout << "fin.\n";
   return 0;
 }
-
-
-/*
- *
-Within function calls
-{
-	Define starting protocols and values (each layer, energy, etc.)
-		Each protocol may need to set its own values (this can be accomplished through a helper class i.e. phy x and y, \
-		  node needs to set its positions, etc.)
-		Must loop through each node assigning its location and other features (can be done mainly through passing to helper \
-		  which then will assign appropriate values using defined functions)
-
-	Set up layout (through mobility i believe)
-	Set up trace (needs to be implemented but a TODO will work for now)
-
-
-	Create traffic generator (ns3::OnOffHelper maybe)
-
-	Start/stop time.
-	Run.
-	Null all nodes (set to 0).
-	Simulation::Destroy().
-}
-
-main
-{
-	Create basics (through function call)
-	Declare any basic variables and use helper to set up
-	call 'Run' function with the given setup from helper (i.e. Helper h; h.SetLayer(x,y,z); h.Run(); OR x = h.Run(); txtOutput(x);)
-}
-
-Notes: Must use containers (i.e. NodeContainers). Can have two different containers (sinks + nodes of same class/type). Use prebuild\
-   terms like 'Create(x)'. Use Simulator::Schedule to do events (sending or other techniques) and/or set callbacks to handle\
-   sending/recving. Error checking is a must!
-
- *
- */
