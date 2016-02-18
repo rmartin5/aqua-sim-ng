0c820aa3e0a2a672fe95f9ec5bb705ff4e4f7d86
rmartin5 <martinr521@gmail.com>
2016-02-18 10:44:10 -0500

    copyright update

--- model/aqua-sim-mac-fama.h
+++ model/aqua-sim-mac-fama.h
@@ -1,8 +1,21 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * aqua-sim-mac-fama.h
+ * Copyright (c) 2016 University of Connecticut
  *
- *  Created on: Feb 17, 2016
- *      Author: Robert Martin
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Robert Martin <robert.martin@engr.uconn.edu>
  */
 
 #ifndef AQUA_SIM_MAC_FAMA_H
